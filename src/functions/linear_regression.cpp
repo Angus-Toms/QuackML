d202614b1f73065fba7ac66ca917a225d86eb62b
Angus-Toms <angus.toms@icloud.com>
2024-01-04 14:14:42 +0000

    Seperate data (in)dependent calcs

--- src/functions/linear_regression.cpp
+++ src/functions/linear_regression.cpp
@@ -4,29 +4,27 @@
 #include <vector>
 #include <iostream>
 
-float get_gradient(std::vector<int> features, std::vector<int> labels, float theta, float lambda) {
+float get_gradient(int feature_count, int Sigma, int C, float theta, float lambda) {
     // Calculate cost function gradient using covariance matrix
-    int feature_count = features.size();
-
-    int Sigma = 0;
-    int C = 0;
-    for (int i = 0; i < feature_count; i++) {
-        Sigma += features[i] * features[i];
-        C += features[i] * labels[i];
-    }
-
     return (1.0 / feature_count) * ((Sigma * theta - C) + (lambda * theta));
 };
 
 float linear_regression_1d(std::vector<int> features, std::vector<int> labels, float alpha, float lambda, int gd_iterations) {
+    int feature_count = features.size();
     // Weight initialization
     float theta = 1.0;
+    // Get covar matrix
+    int Sigma = 0;
+    int C = 0;
+    for (int i=0; i<feature_count; i++) {
+        Sigma += features[i] * features[i];
+        C += features[i] * labels[i];
+    }
 
     // Gradient descent
     for (int i=0; i<gd_iterations; i++) {
         // Calculate gradient of cost function 
-        float gradient = get_gradient(features, labels, theta, lambda);
-
+        float gradient = get_gradient(feature_count, Sigma, C, theta, lambda);
         // Update theta 
         theta = theta - alpha * gradient;
     }
@@ -43,6 +41,6 @@ float linear_regression_1d(std::vector<int> features, std::vector<int> labels, f
 int main() {
     std::vector<int> test_features {1, 2, 3, 4, 5};
     std::vector<int> test_labels {3, 6, 9, 12, 15};
-    float theta = linear_regression_1d(test_features, test_labels, 0.1, 1, 100);
-    std::cout << "Theta: " << theta << std::endl;
+    float theta = linear_regression_1d(test_features, test_labels, 0.1, 0, 100);
+    std::cout << "\nTheta: " << theta << std::endl;
 };
