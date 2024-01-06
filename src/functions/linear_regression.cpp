87d87341db85eac8f1ee5d9cd96c3cc8d1281db6
Angus-Toms <angus.toms@icloud.com>
2024-01-06 13:44:55 +0000

    Change arrays to vectors, linear reg working

--- src/functions/linear_regression.cpp
+++ src/functions/linear_regression.cpp
@@ -38,142 +38,128 @@ float linearRegression1D(std::vector<int> features, std::vector<int> labels, flo
     return theta;
 };
 
-float* matrixScalarMultiply(const float *pMatrix, size_t rows, size_t cols, float scalar, float *pResult) {
+void matrixScalarMultiply(std::vector<std::vector<float>> &matrix, float scalar, std::vector<std::vector<float>> &result) {
     // Multiply each element of a matrix by a scalar
-    for (size_t i=0; i<rows; i++) {
-        for (size_t j=0; j<cols; j++) {
-            pResult[i * cols + j] = pMatrix[i * cols + j] * scalar;
+    for (size_t i=0; i<matrix.size(); i++) {
+        for (size_t j=0; j<matrix[0].size(); j++) {
+            result[i][j] = matrix[i][j] * scalar;
         }
-    }
-    return pResult;
+    };
 };
 
-float* matrixMultiply(const float *pMatrix1, size_t rows1, size_t cols1, const float *pMatrix2, size_t rows2, size_t cols2, float *pResult) {
-    // Matrix multiplication
-    assert(cols1 == rows2);
-
-    for (size_t i=0; i<rows1; i++) {
-        for (size_t j=0; j<cols2; j++) {
-            pResult[i * cols2 + j] = 0;
-            for (size_t k=0; k<cols1; k++) {
-                pResult[i * cols2 + j] += pMatrix1[i * cols1 + k] * pMatrix2[k * cols2 + j];
+void matrixMultiply(std::vector<std::vector<float>> &matrix1, std::vector<std::vector<float>> &matrix2, std::vector<std::vector<float>> &result) {
+    // Multiply two matrices
+    for (size_t i=0; i<matrix1.size(); i++) {
+        for (size_t j=0; j<matrix2[0].size(); j++) {
+            for (size_t k=0; k<matrix2.size(); k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
             }
         }
     }
-    return pResult;
-}
-
-float* matrixSubtract(const float *pMatrix1, const float *pMatrix2, size_t rows, size_t cols, float *pResult) {
-    // Subtract matrix 2 from matrix 1
-    for (size_t i=0; i<rows; i++) {
-        for (size_t j=0; j<cols; j++) {
-            pResult[i * cols + j] = pMatrix1[i * cols + j] - pMatrix2[i * cols + j];
-        }
-    }
-    return pResult;
 };
 
-float* matrixAdd(const float *pMatrix1, const float *pMatrix2, size_t rows, size_t cols, float *pResult) {
-    // Add matrix 2 to matrix 1
-    for (size_t i=0; i<rows; i++) {
-        for (size_t j=0; j<cols; j++) {
-            pResult[i * cols + j] = pMatrix1[i * cols + j] + pMatrix2[i * cols + j];
+void matrixSubtract(std::vector<std::vector<float>> &matrix1, std::vector<std::vector<float>> &matrix2, std::vector<std::vector<float>> &result) {
+    for (size_t i=0; i<matrix1.size(); i++) {
+        for (size_t j=0; j<matrix1[0].size(); j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
         }
     }
-    return pResult;
 };
 
-float* getGradientND(size_t n, size_t d, float **pSigma, float *pC, float *pTheta, float lambda) {
-    // n: number of observations, d: dimensions (variables) in each feature
-    // Calculate cost function gradient using covariance matrix
+void matrixAdd(std::vector<std::vector<float>> &matrix1, std::vector<std::vector<float>> &matrix2, std::vector<std::vector<float>> &result) {
+    for (size_t i=0; i<matrix1.size(); i++) {
+        for (size_t j=0; j<matrix1[0].size(); j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+};
+
+void printMatrix(std::vector<std::vector<float>> &matrix) {
+    for (auto row : matrix) {
+        for (auto element : row) {
+            std::cout << element << ", ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+};
+
+std::vector<std::vector<float>> getGradientND(std::vector<std::vector<float>> &sigma, std::vector<std::vector<float>> &c, std::vector<std::vector<float>> &theta, float lambda) {
+    size_t n = sigma.size();
+    size_t d = sigma[0].size();
     
     // Sigma * theta
-    float *result = new float[d];
-    matrixMultiply(*pSigma, d, d, pTheta, d, 1, result);
+    std::vector<std::vector<float>> result(d, std::vector<float>(1, 0));
+    matrixMultiply(sigma, theta, result);
+
     // Sigma * theta - C
-    matrixSubtract(result, pC, d, 1, result);
+    matrixSubtract(result, c, result);
+
     // (1/n) * (Sigma * theta - C)
-    matrixScalarMultiply(result, d, 1, (1.0 / n), result);
+    matrixScalarMultiply(result, (1.0 / n), result);
+
     // lambda * theta
-    float *pRegularizer = new float[d];
-    matrixScalarMultiply(pTheta, 1, d, lambda, pRegularizer);
+    std::vector<std::vector<float>> regularizer(d, std::vector<float>(1, 0));
+    matrixScalarMultiply(theta, lambda, regularizer);
+
     // (1/n) * (Sigma * theta - C) + (lambda * theta)
-    matrixAdd(result, pRegularizer, d, 1, result);
+    matrixAdd(result, regularizer, result);
+
     return result;
 };
 
-float* linearRegressionND(float **pFeatures, float *pLabels, float alpha, float lambda, size_t iter, size_t n, size_t d) {
-    // Weight initialization
-    float *pTheta = new float[d];
-    std::fill(pTheta, pTheta + d, 1.0);
+void linearRegressionND(std::vector<std::vector<float>> &features, std::vector<std::vector<float>> &labels, float alpha, float lambda, size_t iter) {
+    size_t n = features.size();
+    size_t d = features[0].size();
 
-    // Get covar matrix
-    float **pSigma = new float*[d];
+    std::vector<std::vector<float>> theta(d, std::vector<float>(1, 1.0)); // d, - column vector
+    std::vector<std::vector<float>> sigma(d, std::vector<float>(d, 0)); // d,d - matrix
+    std::vector<std::vector<float>> c(d, std::vector<float>(1, 0)); // d, - column vector
     for (size_t i=0; i<d; i++) {
-        pSigma[i] = new float[d];
-        std::fill(pSigma[i], pSigma[i] + d, 0.0);
-    }
-
-    float *pC = new float[d];
-    std::fill(pC, pC + d, 0.0);
-    for (size_t i=0; i<d; i++) {
-        for (size_t j=i; j<d; j++) {
-            for (size_t k=0; k<n; k++) { // Iterate over n observations
-                pSigma[i][j] += pFeatures[k][i] * pFeatures[k][j];
-                pC[i] += pFeatures[k][i] * pLabels[k];
+        for (size_t k=0; k<n; k++) {
+            c[i][0] += features[k][i] * labels[k][0];
+            for (size_t j=0; j<d; j++) {
+                sigma[i][j] += features[k][i] * features[k][j];
             }
         }
     }
 
-    // Gradient descent 
     for (size_t i=0; i<iter; i++) {
-        auto gradient = getGradientND(n, d, pSigma, pC, pTheta, lambda);
-        matrixScalarMultiply(gradient, d, 1, alpha, gradient);
-        matrixSubtract(pTheta, gradient, d, 1, pTheta);
+        auto gradient = getGradientND(sigma, c, theta, lambda);
+        matrixScalarMultiply(gradient, alpha, gradient);
+        matrixSubtract(theta, gradient, theta);
     }
 
-    // Predictions
     std::cout << "Predictions: ";
-    for (size_t i=0; i<n; i++) {
-        float prediction = 0.0;
-        for (size_t j=0; j<d; j++) {
-            prediction += pFeatures[i][j] * pTheta[j];
+    for (auto feature : features) {
+        float prediction = 0;
+        for (size_t i=0; i<d; i++) {
+            prediction += feature[i] * theta[i][0];
         }
         std::cout << prediction << ", ";
     }
-    return pTheta;
+
+    std::cout << "\nWeights: ";
+    for (size_t i=0; i<d; i++) {
+        std::cout << theta[i][0] << ", ";
+    }
 }
 
 int main() {
-    size_t n = 3;
-    size_t d = 2;
+    std::vector<std::vector<float>> features = {
+        {2, 5},
+        {3, 1},
+        {-1, 4}
+    };
+    float theta1 = -2.486;
+    float theta2 = 0.477;
+    std::vector<std::vector<float>> labels = {
+        {theta1 * features[0][0] + theta2 * features[0][1]},
+        {theta1 * features[1][0] + theta2 * features[1][1]},
+        {theta1 * features[2][0] + theta2 * features[2][1]}
+    };
 
-    float **pFeatures = new float*[n];
-    for (size_t i=0; i<n; i++) {
-        pFeatures[i] = new float[d];
-    }
-    pFeatures[0][0] = 1.0;
-    pFeatures[0][1] = 3.0;
-    pFeatures[1][0] = 9.0;
-    pFeatures[1][1] = 4.0;
-    pFeatures[2][0] = -5.0;
-    pFeatures[2][1] = 2.0;
-
-    float *pLabels = new float[3];
-    pLabels[0] = 9.0;
-    pLabels[1] = 35.0;   
-    pLabels[2] = 11.0;
-
-    auto theta = linearRegressionND(pFeatures, pLabels, 0.1, 0, 10, n, d);
-    
-    std::cout << "\nTheta: ";
-    for (size_t i=0; i<d; i++) {
-        std::cout << theta[i] << ", ";
-    }
-
-    // Deallocate memory
-    for (size_t i=0; i<n; i++) {
-        delete[] pFeatures[i];
-    }
-    delete[] pFeatures;
+    float alpha = 0.01;
+    float lambda = 0.0;
+    linearRegressionND(features, labels, alpha, lambda, 1000);
 };
