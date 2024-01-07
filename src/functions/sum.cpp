d1d4cbbd6b27689e4a2577633135e983dca9e882
Angus-Toms <angus.toms@icloud.com>
2024-01-07 17:14:24 +0000

    Working UDAF in extension. LFG.

---
+++ src/functions/sum.cpp
@@ -0,0 +1,58 @@
+// Redefinition of sum aggregate function
+// Just to get the hang of UDAFs
+
+#include "duckdb/function/function_set.hpp"
+#include "duckdb/parser/parsed_data/create_aggregate_function_info.hpp"
+
+namespace ml {
+
+struct SumState {
+    double sum;
+};
+
+struct SumOperation {
+    template <class STATE>
+    static void Initialize(STATE &state) {
+        state.sum = 0;
+    }
+
+    template <class STATE, class OP>
+    static void Combine(const STATE &source, STATE &target, duckdb::AggregateInputData &) {
+        target.sum += source.sum;
+    }
+
+    template <class T, class STATE>
+    static void Finalize(STATE &state, T &target, duckdb::AggregateFinalizeData &finalize_data) {
+        target = state.sum;
+    }
+
+    template <class INPUT_TYPE, class STATE, class OP>
+    static void Operation(STATE &state, const INPUT_TYPE &input, duckdb::AggregateUnaryInput &unary_input) {
+        state.sum += input; 
+    }
+
+    template <class INPUT_TYPE, class STATE, class OP>
+    static void ConstantOperation(STATE &state, const INPUT_TYPE &input, duckdb::AggregateUnaryInput &unary_input, idx_t count) {
+        for (idx_t i = 0; i < count; i++) {
+            Operation<INPUT_TYPE, STATE, OP>(state, input, unary_input);
+        }
+    }
+
+    static bool IgnoreNull() { 
+        return true; 
+    }
+};
+
+duckdb::AggregateFunction GetSumFunction() {
+    return duckdb::AggregateFunction::UnaryAggregate<SumState, double, double, SumOperation>(
+        duckdb::LogicalType::DOUBLE, duckdb::LogicalType::DOUBLE);
+}
+
+void RegisterSumFunction(duckdb::Connection &conn, duckdb::Catalog &catalog) {
+    duckdb::AggregateFunctionSet my_sum("my_sum");
+    my_sum.AddFunction(GetSumFunction());
+    duckdb::CreateAggregateFunctionInfo my_sum_info(my_sum);
+    catalog.CreateFunction(*conn.context, my_sum_info);
+}
+
+} // namespace ml
